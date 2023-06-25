39953505d09b9a70c1e356fc0f633c9a4ee7a81d
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-06-24 22:37:36 -0300

    add type definition

--- App.tsx
+++ App.tsx
@@ -1,23 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Login from './src/screens/Login';
+import { StackParam } from './src/utils/types';
+
+
+const Stack = createNativeStackNavigator<StackParam>();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Stack.Navigator initialRouteName='Login'>
+        <Stack.Screen name='Login' component={Login} />
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </View>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
