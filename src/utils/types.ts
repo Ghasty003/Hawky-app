450fe3d3c1ad86f1fe6852f0c2e5755b207f99d9
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-06-24 22:38:39 -0300

    type file

---
+++ src/utils/types.ts
@@ -0,0 +1,8 @@
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
+
+export type StackParam = {
+    Login: undefined;
+}
+
+
+export type LoginProp = NativeStackScreenProps<StackParam, "Login">;
