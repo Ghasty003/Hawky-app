10cdb45fa2595363111d8510a800bda3b9b0c480
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-06-25 22:35:43 -0300

    update types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -4,9 +4,11 @@ export type StackParam = {
     Login: undefined;
     Register: undefined;
     Home: undefined;
+    Chats: undefined;
 }
 
 
 export type LoginProp = NativeStackScreenProps<StackParam, "Login">;
 export type RegisterProp = NativeStackScreenProps<StackParam, "Register">;
 export type HomeProp = NativeStackScreenProps<StackParam, "Home">;
+export type ChatsProp = NativeStackScreenProps<StackParam, "Chats">;
