71d726cc4302e1087fb9814906e195380af642b1
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-06-25 21:07:35 -0300

    update types

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -3,8 +3,10 @@ import type { NativeStackScreenProps } from "@react-navigation/native-stack";
 export type StackParam = {
     Login: undefined;
     Register: undefined;
+    Home: undefined;
 }
 
 
 export type LoginProp = NativeStackScreenProps<StackParam, "Login">;
 export type RegisterProp = NativeStackScreenProps<StackParam, "Register">;
+export type HomeProp = NativeStackScreenProps<StackParam, "Home">;
