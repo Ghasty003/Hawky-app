afa7e8695cdadafa4d94102d998a9215a3ad2140
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-06-26 02:18:41 -0300

    fetch user

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
+import React from "react";
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Provider } from "react-native-paper";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 import Login from './src/screens/Login';
 import { StackParam } from './src/utils/types';
@@ -13,10 +15,18 @@ import Chats from './src/screens/Chats';
 const Stack = createNativeStackNavigator<StackParam>();
 
 export default function App() {
+
+  const [user, setUser] = React.useState<string | null>(null);
+
+  React.useEffect(() => {
+    AsyncStorage.getItem("user")
+    .then(u => setUser(u));
+  }, []);
+
   return (
     <NavigationContainer>
       <Provider>
-        <Stack.Navigator initialRouteName='Login'>
+        <Stack.Navigator initialRouteName={user ? "Home" : "Login"}>
           <Stack.Screen name='Login' component={Login} />
           <Stack.Screen name='Register' component={Register} />
           <Stack.Screen name='Home' component={Home} />
