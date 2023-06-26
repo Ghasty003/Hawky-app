1724d8657108123c89a44af29ec0883b9b33d24e
Ajenikoko Gbolahan <gbolahanajenikoko@gmail.com>
2023-06-26 00:42:11 -0300

    update

--- src/screens/Chats.tsx
+++ src/screens/Chats.tsx
@@ -1,15 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { View, Text, Image, TouchableOpacity, StyleSheet, FlatList, TextInput } from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 
 import colors from '../utils/colors';
 import { ChatsProp } from '../utils/types';
 
 function Chats({ navigation }: ChatsProp) {
 
+    const chatRef = useRef<FlatList>(null!);
+
     useEffect(() => {
         navigation.setOptions({
             headerShown: false
         });
+        chatRef.current.scrollToEnd({ animated: true });
     }, []);
 
     const messages = [
@@ -39,15 +43,19 @@ function Chats({ navigation }: ChatsProp) {
         <View style={styles.container}>
             <View style={styles.header}>
                 <View style={styles.imageContainer}>
+                    <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+                        <Icon name='arrow-back-circle-outline' size={25} color="white" />
+                    </TouchableOpacity>
                     <Image style={styles.image} source={require("../../assets/avatar-food.png")} />
                     <Text style={{ color: "white", fontSize: 17}}>John Doe</Text>
                 </View>
                 <TouchableOpacity>
-                    <Text style={{ fontSize: 17, color: "white"}}>Icon</Text>
+                    <Text style={{ fontSize: 17, color: "white"}}><Icon name='search' size={20} /></Text>
                 </TouchableOpacity>
             </View>
 
             <FlatList 
+                ref={chatRef}
                 data={messages}
                 renderItem={({ item }) => (
                     <View style={{ alignItems: item._id === 1 ? "flex-end" : "flex-start"}}>
@@ -66,7 +74,9 @@ function Chats({ navigation }: ChatsProp) {
                  placeholder='Message'
                  style={{ flex: 1, padding: 5, color: "white"}}
                 />
-                <Text>Send</Text>
+                <TouchableOpacity>
+                    <Icon name='send' size={22} color="white" />
+                </TouchableOpacity>
             </View>
         </View>
     );
